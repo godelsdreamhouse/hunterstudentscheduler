cdbccce13636b204ff71b360c592b79532155027
Monae White <72907201+monaewhite@users.noreply.github.com>
2026-05-08 08:15:28 -0400

    Removed redundancies and error-prone types
    course_id is TEXT to ensure digits are immutable. course_short_name and course_long_name are fused into course_name.

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,7 @@
+DROP TYPE IF EXISTS weekday CASCADE;
+DROP TYPE IF EXISTS component CASCADE;
+DROP TYPE IF EXISTS status CASCADE;
+DROP TYPE IF EXISTS modality CASCADE;
 DROP TABLE IF EXISTS section_meetings CASCADE;
 DROP TABLE IF EXISTS sections CASCADE;
 DROP TABLE IF EXISTS course_requirements_map CASCADE;
@@ -9,10 +13,6 @@ DROP TABLE IF EXISTS user_schedules CASCADE;
 DROP TABLE IF EXISTS programs CASCADE;
 DROP TABLE IF EXISTS studentProfiles CASCADE;
 DROP TABLE IF EXISTS user_unavailable_times CASCADE;
-DROP TYPE IF EXISTS weekday CASCADE;
-DROP TYPE IF EXISTS component CASCADE;
-DROP TYPE IF EXISTS status CASCADE;
-DROP TYPE IF EXISTS modality CASCADE;
 
 
 CREATE TYPE weekday AS ENUM ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
@@ -26,10 +26,9 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE courses (
-    course_id INT NOT NULL PRIMARY KEY, -- maps to courseGroupId for scraper 
-    course_short_name TEXT NOT NULL,              -- e.g. 'CSCI 127000 
-    course_long_name TEXT NOT NULL,             -- e.g. 'Introduction to Computer Science'
-    course_code TEXT NOT NULL,                 -- e.g. '127000'
+    course_id TEXT NOT NULL PRIMARY KEY,        -- maps to courseGroupId for scraper 
+    course_code TEXT NOT NULL,              -- e.g. 'CSCI 127000 
+    course_name TEXT NOT NULL,             -- e.g. 'Introduction to Computer Science'
     dep_code TEXT NOT NULL REFERENCES departments(dep_code), -- e.g. 'CSCI'
     course_description TEXT, 
     credits NUMERIC(2,1) NOT NULL DEFAULT 3.0,
@@ -41,11 +40,9 @@ CREATE TABLE courses (
     last_updated DATE              
 ); 
 
-CREATE INDEX idx_courses_subject_number ON courses(dep_code, course_code); -- index for dep_code and course_code lookups
 CREATE INDEX idx_courses_active ON courses(course_id) WHERE is_active = TRUE; -- index for active courses
 
 /* Requirement types (as of 02/09/26):
-   Accounting Minor Core, Accounting Minor Elective, Adolescene Education:  Major Core, Accounting Major Elective,
    CS Major Core, CS Major Elective, Scientific World, Mathematical and Quantitative Reasoning, English Composition,
    Life & Physical Sciences, Creative Expression, U.S. Experiences in its Diversity
    World Cultures and Global Issues, Individual and Society - Social Science,
@@ -59,7 +56,7 @@ CREATE TABLE course_requirements (
 );
 
 CREATE TABLE course_requirements_map (
-    course_id INT NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE,
+    course_id TEXT NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE,
     req_id    BIGINT NOT NULL REFERENCES course_requirements(req_id) ON DELETE CASCADE,
     PRIMARY KEY (course_id, req_id)
 );
@@ -67,7 +64,7 @@ CREATE TABLE course_requirements_map (
 CREATE TABLE sections (
     section_id BIGSERIAL PRIMARY KEY,
     class_num INT NOT NULL,
-    course_id INT NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE,
+    course_id TEXT NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE,
     term_season TEXT NOT NULL CHECK (term_season IN ('SPRING', 'SUMMER', 'FALL', 'WINTER')),
     term_year INT NOT NULL CHECK (term_year >= 2000 AND term_year <= 2100),
     section_number TEXT NOT NULL,                  -- e.g. 01, 02, L1
@@ -171,7 +168,7 @@ CREATE TABLE user_unavailable_times (
 );
 
 
-SELECT c.course_id, c.course_short_name as title, c.course_description, t.req_name
+SELECT c.course_id, c.course_name as title, c.course_description, t.req_name
 FROM course_requirements_map m
 JOIN courses c ON c.course_id = m.course_id
 JOIN course_requirements t ON t.req_id = m.req_id
