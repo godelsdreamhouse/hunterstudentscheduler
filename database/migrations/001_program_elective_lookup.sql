6a53a0eb8190fd0ac99b00f5f6846113ce003c78
Monae White <72907201+monaewhite@users.noreply.github.com>
2026-05-19 10:31:59 -0400

    Added cs major core course into the program elective exclusions

--- database/migrations/001_program_elective_lookup.sql
+++ database/migrations/001_program_elective_lookup.sql
@@ -1,4 +1,8 @@
--- Seed supported programs. Schema tables already exist.
+-- Seed supported programs and their elective rules based on:
+-- put("ComputerScience_ComputerScience", new String[] {"CSCI >13600", "CSCI 49600, 49700, 49800, 49900, 12000, 12100, 13200, 13300, 18100, 18200, 18300, 22700, 23200, 23300"});
+-- put("Mathematics_Mathematics", new String[] {"MATH 3@ or MATH 4@ or STAT 3@ or STAT 4@", ""});
+-- put("PoliticalScience_None", new String[] {"POLSC 4@ or POLSC 3@ or POLSC 2@ or POLSC 1@", ""});
+
 INSERT INTO programs (
     program_key,
     program_name,
@@ -92,12 +96,20 @@ JOIN (
         ('CSCI 12100'),
         ('CSCI 13200'),
         ('CSCI 13300'),
+        ('CSCI 13500'),
+        ('CSCI 15000'),
+        ('CSCI 16000'),
         ('CSCI 18100'),
         ('CSCI 18200'),
         ('CSCI 18300'),
         ('CSCI 22700'),
         ('CSCI 23200'),
-        ('CSCI 23300')
+        ('CSCI 23300'),
+        ('CSCI 23500'),
+        ('CSCI 26000'),
+        ('CSCI 26500'),
+        ('CSCI 33500'),
+        ('CSCI 34000')
 ) AS e(course_code)
     ON p.program_key = 'ComputerScience_ComputerScience'
 ON CONFLICT (program_id, course_code) DO NOTHING;
