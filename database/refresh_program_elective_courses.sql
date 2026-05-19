e6e32897b57b98e07737cc73cccacc5d6a9299ce
Monae White <72907201+monaewhite@users.noreply.github.com>
2026-05-19 14:22:32 -0400

    Revert "Add program elective lookup migration"
    This reverts commit 51d0f68a0d0b218acc43283195e9c023a8124858.

--- database/refresh_program_elective_courses.sql
+++
@@ -1,4 +0,0 @@
--- Rebuilds the lookup used by the preferences page smart search.
-
--- Run this after the scraper updates courses or after elective rules change.
-REFRESH MATERIALIZED VIEW program_elective_courses;
